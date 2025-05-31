cef3ba2594b5aa669138cea2f1dba06af1857912
akshy-007 <akshaytakale71@gmail.com>
2025-05-31 15:26:42 +0530

    Initial commit

---
+++ MS_4_ServiceRegistry/src/main/java/com/example/MS_4_ServiceRegistry/Ms4ServiceRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.example.MS_4_ServiceRegistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ms4ServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ms4ServiceRegistryApplication.class, args);
+	}
+
+}
---
+++ MS_4_ServiceRegistry/src/test/java/com/example/MS_4_ServiceRegistry/Ms4ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MS_4_ServiceRegistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ms4ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
